7cbabfbe139ba3a6395850b0d18bd0b900788b34
Brinthapan <brinthapanj@gmail.com>
2024-08-06 16:10:01 +0530

    project upload

---
+++ bbdms/about.php
@@ -0,0 +1,148 @@
+<?php
+error_reporting(0);
+include('includes/config.php');
+?>
+
+<!DOCTYPE html>
+<html lang="zxx">
+
+<head>
+	<title>Blood Bank Donar Management System | About Us </title>
+	<!-- Meta tag Keywords -->
+	
+	<script>
+		addEventListener("load", function () {
+			setTimeout(hideURLbar, 0);
+		}, false);
+
+		function hideURLbar() {
+			window.scrollTo(0, 1);
+		}
+	</script>
+	<!--// Meta tag Keywords -->
+
+	<!-- Custom-Files -->
+	<link rel="stylesheet" href="css/bootstrap.css">
+	<!-- Bootstrap-Core-CSS -->
+	<link rel="stylesheet" href="css/style.css" type="text/css" media="all" />
+	<!-- Style-CSS -->
+	<link rel="stylesheet" href="css/fontawesome-all.css">
+	<!-- Font-Awesome-Icons-CSS -->
+	<!-- //Custom-Files -->
+
+	<!-- Web-Fonts -->
+	<link href="//fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i&amp;subset=cyrillic,cyrillic-ext,greek,greek-ext,latin-ext,vietnamese"
+	    rel="stylesheet">
+	<link href="//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i&amp;subset=cyrillic,cyrillic-ext,greek,greek-ext,latin-ext,vietnamese"
+	    rel="stylesheet">
+	<!-- //Web-Fonts -->
+
+</head>
+
+<body>
+	<?php include('includes/header.php');?>
+
+	<!-- banner 2 -->
+	<div class="inner-banner-w3ls">
+		<div class="container">
+
+		</div>
+		<!-- //banner 2 -->
+	</div>
+	<!-- page details -->
+	<div class="breadcrumb-agile">
+		<div aria-label="breadcrumb">
+			<ol class="breadcrumb">
+				<li class="breadcrumb-item">
+					<a href="index.php">Home</a>
+				</li>
+				<li class="breadcrumb-item active" aria-current="page">About Us</li>
+			</ol>
+		</div>
+	</div>
+	<!-- //page details -->
+
+	<!-- about -->
+	<section class="about py-5">
+		<div class="container py-xl-5 py-lg-3">
+			<?php 
+$pagetype="aboutus";
+$sql = "SELECT type,detail,PageName from tblpages where type=:pagetype";
+$query = $dbh -> prepare($sql);
+$query->bindParam(':pagetype',$pagetype,PDO::PARAM_STR);
+$query->execute();
+$results=$query->fetchAll(PDO::FETCH_OBJ);
+$cnt=1;
+if($query->rowCount() > 0)
+{
+foreach($results as $result)
+{ ?>
+			<div class="w3ls-titles text-center mb-md-5 mb-4">
+				<h3 class="title"><?php   echo htmlentities($result->PageName); ?></h3>
+				<span>
+					<i class="fas fa-user-md"></i>
+				</span>
+			</div>
+			<!-- <p class="aboutpara text-center mx-auto"><?php  echo $result->detail; ?>.</p> -->
+<?php } } ?>
+			
+		</div>
+	</section>
+	<!-- //about -->
+
+	<p style="margin-left: 150px; margin-right: 150px; text-align: justify;">At Blood Donor Management, our mission is to facilitate and streamline the process of blood donation to ensure a steady supply of safe and life-saving blood for those in need. We understand the critical role that blood plays in medical emergencies, surgeries, and various medical conditions, and we are committed to supporting the healthcare system by connecting donors with patients.
+
+Our platform serves as a bridge between blood donors and healthcare institutions, making it easier for individuals to donate blood and for medical facilities to access the required blood units. We prioritize the safety and well-being of both donors and recipients by adhering to stringent quality control measures and following established medical guidelines.
+
+Through our user-friendly website and mobile application, potential donors can register their profiles, providing essential information such as blood type, contact details, and availability. This information helps us match donors with specific patient needs, ensuring a timely and efficient blood supply.</p>
+<br>
+	<?php include('includes/footer.php');?>
+
+
+	<!-- Js files -->
+	<!-- JavaScript -->
+	<script src="js/jquery-2.2.3.min.js"></script>
+	<!-- Default-JavaScript-File -->
+
+	<!-- banner slider -->
+	<script src="js/responsiveslides.min.js"></script>
+	<script>
+		$(function () {
+			$("#slider4").responsiveSlides({
+				auto: true,
+				pager: true,
+				nav: true,
+				speed: 1000,
+				namespace: "callbacks",
+				before: function () {
+					$('.events').append("<li>before event fired.</li>");
+				},
+				after: function () {
+					$('.events').append("<li>after event fired.</li>");
+				}
+			});
+		});
+	</script>
+	<!-- //banner slider -->
+
+	<!-- fixed navigation -->
+	<script src="js/fixed-nav.js"></script>
+	<!-- //fixed navigation -->
+
+	<!-- smooth scrolling -->
+	<script src="js/SmoothScroll.min.js"></script>
+	<!-- move-top -->
+	<script src="js/move-top.js"></script>
+	<!-- easing -->
+	<script src="js/easing.js"></script>
+	<!--  necessary snippets for few javascript files -->
+	<script src="js/medic.js"></script>
+
+	<script src="js/bootstrap.js"></script>
+	<!-- Necessary-JavaScript-File-For-Bootstrap -->
+
+	<!-- //Js files -->
+
+</body>
+
+</html>
